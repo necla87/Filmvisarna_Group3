b113c7940233cf6bbce66e34c4abf155efe26444
necla <123477628+necla87@users.noreply.github.com>
2024-01-30 10:53:40 +0100

    moved on coding

--- specs/step_definitions/avboka-bokning.js
+++ specs/step_definitions/avboka-bokning.js
@@ -11,19 +11,39 @@ When('I click on {string} tab from the header', (BOKA) => {
 });
 
 Then('I select the movie, the date and the seats', () => {
-  cy.get(':nth-child(3) > select').select('Titane');
-  cy.get(':nth-child(4) > select').should('have.value', '49')
+  // cy.get(':nth-child(3) > select').select('Titane');
+  // cy.get(':nth-child(4) > select').should('have.value', '49')
+  // Select the movie
+  cy.get('.dropdown-container:nth-child(3) select').select('Titane');
+  // Select the week
+  cy.wait(2000)
+  cy.get('.dropdown-container:nth-child(4) select').select('Vecka 49');
+  cy.wait(2000)
+  // Select the showing
+  cy.get('.dropdown-container:nth-child(5) select').select('6523d5601451567f3ed4cec9');
+  cy.wait(3000)
+  cy.get('.ticket-counter-container').contains('Vuxenbiljetter').next('.plus-minus-container').find('.ticket-counter-arrow-plus').click();
+  cy.get('.ticket-counter-container').contains('Vuxenbiljetter').next('.plus-minus-container').find('.ticket-counter-arrow-plus').click();
 
- 
+  cy.get('#\\33').click();
+  
+  cy.get('#\\34').click();
+
 
 });
 
 Then('I complete the booking by pressing {string} button', (a) => {
-  // TODO: implement step
+  cy.get('.book-button').click();
+  cy.get('.email-field').type('necla.saglam87@gmail.com');
+  cy.get('.send-btn').click();
+  cy.get('.confirm-btn').click();
+
 });
 
 When('I get back to the home page', () => {
   // TODO: implement step
+  cy.get('.nav-btn').click();
+
 });
 
 Then('there is no option to cancel the booking', () => {
