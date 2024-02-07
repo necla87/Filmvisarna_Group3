4272224bb83d2c6e6c9ff1a78d2df9845dd5b8b5
SaraSorlin <sorlin82@gmail.com>
2024-02-07 19:03:16 +0100

    changes

--- specs/step_definitions/visa-trailer.js
+++ specs/step_definitions/visa-trailer.js
@@ -14,36 +14,34 @@ When('the user press on the movie title', () => {
   cy.get('a.link-color').contains('Parasit').click()
 });
 
+const getIframeDocument = () => {
+  return cy
+    .get('iframe[title="Trailer"]')
+    // Cypress yields jQuery element, which has the real
+    // DOM element under property "0".
+    // From the real DOM iframe element we can get
+    // the "document" element, it is stored in "contentDocument" property
+    // Cypress "its" command can access deep properties using dot notation
+    // https://on.cypress.io/its
+    .its('0.contentDocument').should('exist')
+}
+
+const getIframeBody = () => {
+  // get the document
+  return getIframeDocument()
+    // automatically retries until body is loaded
+    .its('body').should('not.be.undefined')
+    // wraps "body" DOM element to allow
+    // chaining more Cypress commands, like ".find(...)"
+    .then(cy.wrap)
+}
+
 Then('the user click on the red play button', () => {
   // TODO: implement step
-  const getIframeDocument = () => {
-    return cy
-      .get('iframe[title="Trailer"]')
-      // Cypress yields jQuery element, which has the real
-      // DOM element under property "0".
-      // From the real DOM iframe element we can get
-      // the "document" element, it is stored in "contentDocument" property
-      // Cypress "its" command can access deep properties using dot notation
-      // https://on.cypress.io/its
-      .its('0.contentDocument').should('exist')
-  }
-
-  const getIframeBody = () => {
-    // get the document
-    return getIframeDocument()
-      // automatically retries until body is loaded
-      .its('body').should('not.be.undefined')
-      // wraps "body" DOM element to allow
-      // chaining more Cypress commands, like ".find(...)"
-      .then(cy.wrap)
-  }
-
-
-
-
+  getIframeBody().find('.ytp-large-play-button').click();
 });
 
 Then('the user should be able to see the trailer for the movie', () => {
   // TODO: implement step
-  getIframeBody().find('.ytp-large-play-button').click();
+
 });
